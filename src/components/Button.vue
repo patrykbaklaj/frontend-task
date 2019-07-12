e0eb33d377140bb96aa326627ee0c47e959accd5
patrykbaklaj@gmail.com <patrykbaklaj@gmail.com>
2019-07-12 17:53:06 +0100

    Create Button Component
    Change Button Component file name && implement Button to EmployeesList Component
    
    Add Button Component styles
    
    Connect Button Component action with EmployeesList
    
    Task 1: Create Button Component and Use it in EmloyeezList Component

---
+++ src/components/Button.vue
@@ -0,0 +1,65 @@
+<template>
+    <button class="button" @click="dispatchEvent(id)">{{value}}</button>
+</template>
+
+<script>
+export default {
+    // get props
+    props: ["value", "id", "className"],
+
+    // define methods
+    methods: {
+        // define method to handle button click event and delegate it to parent element
+        dispatchEvent(id) {
+            this.$emit("buttonClicked", id);
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+    cursor: pointer;
+    border: 1px solid;
+    background-color: transparent;
+    border-radius: 5px;
+    outline: none;
+    padding: 0.4em 1.2em;
+    transition: all 0.25s;
+
+    &:hover {
+        background-color: #000;
+        color: #fff;
+    }
+
+    &-edit {
+        border-color: #ff9800;
+        color: #ff9800;
+
+        &:hover {
+            background-color: #ff9800;
+            color: white;
+        }
+    }
+
+    &-success {
+        border-color: #24bd31;
+        color: #24bd31;
+
+        &:hover {
+            background-color: #24bd31;
+            color: white;
+        }
+    }
+
+    &-danger {
+        border-color: red;
+        color: red;
+
+        &:hover {
+            background-color: red;
+            color: white;
+        }
+    }
+}
+</style>
--- src/pages/EmployeesList.vue
+++ src/pages/EmployeesList.vue
@@ -9,82 +9,104 @@
                 <th>Address</th>
                 <th>Phone</th>
                 <th>Email</th>
+                <th>Actions</th>
             </tr>
-            <tr v-for="employee in employees" class="employees-list__list-row">
+            <tr v-for="employee in employees" class="employees-list__list-row" :key="employee.id">
                 <td>{{employee.id}}</td>
                 <td>{{employee.name}}</td>
                 <td>{{employee.address.street}} {{employee.address.suite}} {{employee.address.city}}</td>
                 <td>{{employee.phone}}</td>
-                <td><a :href="`mailto:${ employee.email }`">{{employee.email}}</a></td>
+                <td>
+                    <a :href="`mailto:${ employee.email }`">{{employee.email}}</a>
+                </td>
+                <td>
+                    <Button
+                        value="edit"
+                        class="button-edit"
+                        :id="employee.id"
+                        @buttonClicked="handleEditClick"
+                    ></Button>
+                </td>
             </tr>
         </table>
     </div>
 </template>
 <script>
-    import axios from 'axios';
+import axios from "axios";
+// import Button Component
+import Button from "@components/Button";
 
-    export default {
-        data() {
-            return {
-                loading: false,
-                employees: [],
-            }
-        },
-        created () {
-            this.fetchData()
-        },
-        watch: {
-            '$route': 'fetchData'
-        },
-        methods: {
-            fetchData () {
-                this.loading = true;
+export default {
+    data() {
+        return {
+            loading: false,
+            employees: []
+        };
+    },
+    created() {
+        this.fetchData();
+    },
+    watch: {
+        $route: "fetchData"
+    },
+    methods: {
+        fetchData() {
+            this.loading = true;
 
-                axios.get('https://jsonplaceholder.typicode.com/users')
-                    .then(({data}) => {
-                        this.employees = data;
-                    })
-                    .finally(() => {
-                        this.loading = false;
-                    })
-            }
+            axios
+                .get("https://jsonplaceholder.typicode.com/users")
+                .then(({ data }) => {
+                    this.employees = data;
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+        // method to handle button Click
+        handleEditClick(id) {
+            // TODO
+            // handle Edit Button click
+            console.log("Emp ID: " + id);
         }
-    };
-
+    },
+    components: {
+        Button
+    }
+};
 </script>
 <style lang="scss" scoped>
-    .employees-list {
-        &__header {
-            font-size: 20px;
-            padding: 0 0 10px;
-        }
+.employees-list {
+    &__header {
+        font-size: 20px;
+        padding: 0 0 10px;
+    }
 
-        &__loading {
-            color: #999d9d;
-            text-align: center;
-        }
+    &__loading {
+        color: #999d9d;
+        text-align: center;
+    }
 
-        &__list {
-            font-size: 14px;
-            width: 100%;
-            border-collapse: collapse;
-        }
+    &__list {
+        font-size: 14px;
+        width: 100%;
+        border-collapse: collapse;
+    }
 
-        &__list-header {
-            background: #f7f8f9;
-            border-bottom: 1px solid #999d9d;
+    &__list-header {
+        background: #f7f8f9;
+        border-bottom: 1px solid #999d9d;
 
-            th {
-                padding: 8px;
-            }
-        }
-
-        &__list-row {
-            background: #fff;
-
-            td {
-                padding: 8px;
-            }
+        th {
+            padding: 8px;
         }
     }
+
+    &__list-row {
+        background: #fff;
+
+        td {
+            padding: 8px;
+        }
+    }
+}
 </style>
